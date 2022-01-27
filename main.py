feac75a389ee1a7078833ee4bf9592d014f7bd15
HarshRathi1 <harsh.rathi20001@gmail.com>
2022-01-27 23:08:37 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import *
+app=Flask(__name__)
+app.secret_key="harsh"
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/success',methods=['POST'])
+def success():
+    if request.method=='POST':
+        session['email']=request.form['email']
+    return render_template('success.html')
+@app.route('/profile')
+def profile():
+    if 'email' in session:
+        email=session['email']
+        return render_template('profile.html',name=email)
+    else:
+        return '<p> Please login first </p>'
+if __name__=='__main__':
+    app.run(debug=True)
